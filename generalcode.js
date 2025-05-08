aa5c478c8718d77a38f925ce03d9e1472214097d
unknown <felix.it@yahoo.com>
2025-05-08 17:39:02 +0200

    update to style hadling

--- generalcode.js
+++ generalcode.js
@@ -2,8 +2,7 @@ $(document).ready(function main(){
     $(".stylechange").click(function change(){
         let classtoadd = $(this).attr("data-stylemode");
         let objectaffected = $(this).attr("data-object");
-        console.log(classtoadd)
-        $(objectaffected).attr('class',classtoadd);
+        $(objectaffected).attr('id',classtoadd);
         //https://stackoverflow.com/questions/1424981/how-can-i-remove-all-css-classes-using-jquery-javascript
        //https://www.w3schools.com/jquery/jquery_css_classes.asp
         
