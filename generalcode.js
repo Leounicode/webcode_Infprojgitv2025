338824e052a1e6db7471232e18f4a60ea303464e
unknown <felix.it@yahoo.com>
2025-05-11 21:33:57 +0200

    changing style systems with more options

--- generalcode.js
+++ generalcode.js
@@ -1,8 +1,9 @@
 $(document).ready(function main(){
     $(".stylechange").click(function change(){
-        let classtoadd = $(this).attr("data-stylemode");
+        let classtoadd = $(this).attr("data-attrvalue");
         let objectaffected = $(this).attr("data-object");
-        $(objectaffected).attr('id',classtoadd);
+        let attrbute = $(this).attr("data-attr");
+        $(objectaffected).attr(attrbute,classtoadd);
         //https://stackoverflow.com/questions/1424981/how-can-i-remove-all-css-classes-using-jquery-javascript
        //https://www.w3schools.com/jquery/jquery_css_classes.asp
         
