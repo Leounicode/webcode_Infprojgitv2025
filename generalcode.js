82151e25c35a9f414202e6ae441cfefafd04241c
Leounicode <felix.it@yahoo.com>
2025-05-13 09:43:14 +0200

    Borders

--- generalcode.js
+++ generalcode.js
@@ -1,4 +1,5 @@
 $(document).ready(function main(){
+
     $(".stylechange").click(function change(){
         let classtoadd = $(this).attr("data-attrvalue");
         let objectaffected = $(this).attr("data-object");
